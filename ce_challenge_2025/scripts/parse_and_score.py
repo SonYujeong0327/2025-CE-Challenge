24316e3b38e7ecd9a2cd1c1636d599de1543e8b2
cechallenge <cechallenge>
2025-07-24 09:57:06 +0900

    update test script

--- ce_challenge_2025/scripts/parse_and_score.py
+++ ce_challenge_2025/scripts/parse_and_score.py
@@ -29,14 +29,14 @@ with open(reftime_path, "r") as reffile:
 
 if len(candidate_times) != len(ref_times):
     print("ERROR: Result and Reference Time mismatch. Check the reftime file has same number of entries as the result file")
-    exit
+    sys.exit(1)
 
 speedup_list = []
 geomean = 1.0
 for candidate, ref in zip(candidate_times, ref_times):
     if (ref == 0.0 or candidate == 0.0):
         print("ERROR: Can't get geomean when ther is a zero value")
-        exit
+        sys.exit(1)
     speedup = ref/candidate
     geomean = geomean * speedup
     speedup_list.append(speedup)
